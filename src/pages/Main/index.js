5aeaedc4906132192f0d476ba49a436bf2aab0c0
eduardonk9999 <thiago.novak7@gmail.com>
2021-02-24 17:17:39 -0300

    Add, project reposito

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import api from '../../services/api';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
-export default function Main() {
-  return (
-    <>
-      <Title>Main</Title>
-      <small>Tes</small>
-    </>
-  );
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  // carregar os dados do localstorage
+  componentDidMount() {
+    
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    }
+    
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false
+    })
+  };
+
+  render() {
+    const { newRepo, repositories, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            { loading ? <FaSpinner color="#FFF" size={14} /> : <FaPlus color="#FFF" size={14} /> }
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repositorie => (
+            <li key={repositorie.name}>
+              <span>{repositorie.name}</span>
+              <a href="#">Detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
